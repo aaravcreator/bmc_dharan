4781ff158545a6634597858b57df0c7f28672928
Aarav Poudel <poudelaarav@gmail.com>
2023-07-23 17:23:29 +0545

    Inital Commit

---
+++ bmc_site/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for bmc_site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from bmc_site import views   # views.index
+# from .views import * # index
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("mypage",views.index),
+    path('display_name/<str:name>/',views.display_name,name ="display_name"),
+    path('',views.display_page,name="display_page")
+]
---
+++ bmc_site/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import HttpResponse,render
+
+
+def index(request):
+    return HttpResponse("THIS IS A RESPONSE")
+
+
+def display_name(request,name):
+    return HttpResponse('Your name is {}'.format(name))
+
+
+def display_page(request):
+    context ={
+        'title':"Welcome to my website",
+        'mydata': [
+            "football",
+            "volleyball",
+            "basketball"
+        ]
+    }
+    return render(request,'index.html',context)
